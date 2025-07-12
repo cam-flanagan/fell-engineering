6bfd65c7802ae08915e8ca1faf303353fd68bbec
cam-flanagan <cam.flanagan15@gmail.com>
2025-07-12 15:25:38 +0100

    call to action

--- src/slices/Showcase/index.tsx
+++ src/slices/Showcase/index.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Bounded from "@/components/Bounded";
-import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicNextImage } from "@prismicio/next";
 import ButtonLink from "@/components/ButtonLink";
 import clsx from "clsx";
 import { PiArrowsClockwise, PiGear } from "react-icons/pi";
